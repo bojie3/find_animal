5d053bb4f4bd32db4798b00c67e6e592449e9659
TanWenCong <105650536+TanWenCong00@users.noreply.github.com>
2023-01-05 14:14:14 +0800

    Add single comment box
    Add HTML and CSS for single comment box, includes sample profile picture and comment.

---
+++ frontend/src/components/CommentSection/CommentSectionElements.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+
+export const NewCommentContainer = styled.textarea`
+`;
+
+export const CommentSectionContainer = styled.div`
+    background-color: #AEDBF0;
+    padding: 5vh 5vw;
+    border-top: 40px solid white;
+`;
+
+export const CommentContainer = styled.div`
+    height: 30vh;
+    background-color: #FFF;
+`;
+
+export const UserDetailsContainer = styled.div`
+    display: flex;
+    background-color: #FFF;
+    align-items: center;
+`;
+
+export const ProfilePic = styled.img`
+    height: 10vh;
+    border-radius: 50%;
+    margin: 1vw;
+`;
+
+export const Username = styled.div`
+`;
+
+export const Comment = styled.div`
+    padding: 1vh 15vh;
+`;
--- frontend/src/components/CommentSection/index.js
+++ frontend/src/components/CommentSection/index.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import { 
+  NewCommentContainer,
+  CommentSectionContainer,
+  CommentContainer,
+  UserDetailsContainer,
+  ProfilePic,
+  Username,
+  Comment
+ } from './CommentSectionElements'
+import UserProfilePic from '../../images/UserProfilePic.png'
 
 const CommentSection = () => {
   return (
-    <div>CommentSection</div>
+    <>
+      <NewCommentContainer>
+        New Comment here!
+      </NewCommentContainer>
+      <CommentSectionContainer>
+        <CommentContainer>
+          <UserDetailsContainer>
+            <ProfilePic src={UserProfilePic} />
+            <Username>Pengwing</Username>
+          </UserDetailsContainer>
+          <Comment>Penguins have knees!</Comment>
+        </CommentContainer>
+      </CommentSectionContainer>
+    </>
   )
 }
 
